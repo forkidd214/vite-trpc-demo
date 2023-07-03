6cfefc9b0101fd3e013d687af7fd591d3553a65b
jasonfzou <forkidd214@gmail.com>
2023-07-04 01:49:30 +0930

    refactor(apps/client): apps/client refers to local eslint-config-custom for eslint config

--- apps/client/.eslintrc.cjs
+++ apps/client/.eslintrc.cjs
@@ -1,12 +1,7 @@
 module.exports = {
+  root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  extends: ['custom', 'plugin:react-hooks/recommended'],
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
