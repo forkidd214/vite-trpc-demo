bee667e7359688fd1d65cfc83455e38e81d3776d
jasonfzou <forkidd214@gmail.com>
2023-07-03 18:36:16 +0930

    refactor: update global and shared packages, which are referred to apps/server
    BREAKING CHANGE: downgrade typescript to 5.0.4 to make eslint happy

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,18 +2,19 @@ module.exports = {
   env: {
     node: true,
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaVersion: 'latest',
   },
   rules: {
-    "@typescript-eslint/no-non-null-assertion": "off",
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
-};
+  ignorePatterns: ['node_modules', 'dist'],
+}
